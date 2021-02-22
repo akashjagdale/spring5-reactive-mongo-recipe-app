8a5891c11c9b47a7a67f8e9af713dd5f6e2ef889
akashjagdale <adjagdale25@gmail.com>
2021-02-22 17:27:34 +0530

    Refactor Recipe Service

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -10,9 +10,9 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
+import reactor.core.publisher.Flux;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -52,18 +52,13 @@ public class IndexControllerTest {
     @Test
     public void getIndexPage() throws Exception {
 
-        //given
-        Set<Recipe> recipes = new HashSet<>();
-        recipes.add(new Recipe());
-
         Recipe recipe = new Recipe();
         recipe.setId("1");
 
-        recipes.add(recipe);
 
-        when(recipeService.getRecipes()).thenReturn(recipes);
+        when(recipeService.getRecipes()).thenReturn(Flux.just(recipe, new Recipe()));
 
-        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+        ArgumentCaptor<Flux<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Flux.class);
 
         //when
         String viewName = controller.getIndexPage(model);
@@ -73,7 +68,7 @@ public class IndexControllerTest {
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
         verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
-        Set<Recipe> setInController = argumentCaptor.getValue();
+        List<Recipe> setInController = argumentCaptor.getValue().collectList().block();
         assertEquals(2, setInController.size());
     }
 
